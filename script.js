3f21c4b80e05848f9ac11901e62f7bd9c929dfd3
Raunak <raunak@spak.com>
2024-12-03 19:28:40 +0530

    today's changes

--- script.js
+++ script.js
@@ -1,125 +1,26 @@
+// create a map() in javascript
+const fruits = new Map([
+    ['Apple', 500],
+    ['Banana', 60],
+    ['Orange', 100],
+]);
 
-// if statement
-if (new Date().getHours() < 18) {
-    document.getElementById("demo").innerHTML += "1 Good day!" + "<br>";
-}
+console.log(fruits);
 
-// else statement
-const hour = new Date().getHours();
-let greeting;
+// regular expression
+const text = "Hello World!";
+const regex = /world/i;
 
-if (hour < 18) {
-    greeting = "Good day";
-} else {
-    greeting = "Good evening";
-}
-document.getElementById("demo").innerHTML += 2 + " " + greeting + "<br>";
+console.log(regex.test(text));
 
 
-//else if statement
-const time = new Date().getHours();
-let msg;
-if (time < 10) {
-    msg = "Good morning";
-} else if (time < 20) {
-    msg = "Good day";
-} else {
-    msg = "Good evening";
-}
-document.getElementById("demo").innerHTML += msg + "<br>";
-
-
-// switch statement
-let day;
-switch (new Date().getDay()) {
-    case 0:
-        day = "Sunday";
-        break;
-    case 1:
-        day = "Monday";
-        break;
-    case 2:
-        day = "Tuesday";
-        break;
-    case 3:
-        day = "Wednesday";
-        break;
-    case 4:
-        day = "Thursday";
-        break;
-    case 5:
-        day = "Friday";
-        break;
-    case 6:
-        day = "Saturday";
-}
-document.getElementById("demo").innerHTML += "Today is " + day + "<br>";
-
-
-// default statement
-let text;
-switch (new Date().getDay()) {
-  case 6:
-    text = "Today is Saturday";
-    break;
-  case 0:
-    text = "Today is Sunday";
-    break;
-  default:
-    text = "Looking forward to the Weekend";
-}
-document.getElementById("demo").innerHTML += text + "<br>";
-
-// Loop Example
-// for loop example
-let a = "";
-
-for (let i = 0; i < 5; i++) {
-  a += "The number is " + i + "<br>";
-}
-document.getElementById("demo").innerHTML += a;
-
-
-
-// while loop
-let count = 1;
-
-while (count <= 5) {
-    console.log(`Count is: ${count}`);
-    count++;
-}
-
-// do while loop
-let increase = 1;
-
-do {
-    console.log(`Count is: ${increase}`);
-    increase++;
-} while (increase <= 5);
-
-
-// break and continue
-
-// break statement
-for (let i = 1; i <= 10; i++) {
-    if (i === 5) {
-        console.log("Breaking the loop at:", i);
-        break; // Exits the loop when i equals 5
-    }
-    console.log(i);
-}
-
-// continue
-for (let i = 1; i <= 10; i++) {
-    if (i === 5) {
-        console.log("Skipping:", i);
-        continue; // Skips the current iteration when i equals 5
-    }
-    console.log(i);
-}
-
-// iterable example
-const myArray = [1, 2, 3];
-for (const value of myArray) {
-    console.log(value); 
+// try, catch & throw block in js example
+try {
+    let jsonData = '{"name": "John", "age": 30}';
+    let user = JSON.parse(jsonData);
+    console.log(user.name); 
+} catch (error) {
+    console.log("Invalid JSON data");
+} finally {
+    console.log("Parsing complete");
 }
